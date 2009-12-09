3dd120c706d6baf7f8708561b82697174b8dd27c
Joshua Peek <josh@joshpeek.com>
2009-12-09 09:26:06 -0600

    Encapsulate asset structure

--- lib/asset_server.rb
+++ lib/asset_server.rb
@@ -3,12 +3,43 @@ require 'time'
 
 module Assets
   class BundleServer
-    def initialize(*paths)
-      @sources = paths.map { |path| Dir[path] }.flatten
+    class Asset
+      attr_reader :source, :md5, :etag, :created_at
+
+      def initialize(sources)
+        @sources    = sources
+        @source     = concate_source
+        @md5        = compute_md5
+        @etag       = quoted_md5
+        @created_at = last_modified
+
+        freeze
+      end
+
+      def stale?
+        @created_at < last_modified
+      end
+
+      private
+        def concate_source
+          @sources.collect { |file| File.read(file) }.join("\n\n")
+        end
+
+        def compute_md5
+          Digest::MD5.hexdigest(@source)
+        end
+
+        def quoted_md5
+          %("#{@md5}")
+        end
+
+        def last_modified
+          @sources.collect { |file| File.mtime(file) }.sort.last
+        end
     end
 
-    def last_modified_source
-      @sources.collect { |file| File.mtime(file) }.sort.last
+    def initialize(*paths)
+      @sources = paths.map { |path| Dir[path] }.flatten
     end
 
     def call(env)
@@ -25,15 +56,11 @@ module Assets
 
     private
       def source_changed?
-        @previous_last_modified.nil? ||
-          (@previous_last_modified < last_modified_source)
+        @asset.nil? || @asset.stale?
       end
 
       def rebundle
-        @source                 = concate_source
-        @md5                    = compute_md5
-        @etag                   = quoted_md5
-        @previous_last_modified = last_modified_source
+        @asset = Asset.new(@sources)
       end
 
 
@@ -42,11 +69,11 @@ module Assets
       end
 
       def not_modified?(env)
-        env["HTTP_IF_MODIFIED_SINCE"] == @previous_last_modified.httpdate
+        env["HTTP_IF_MODIFIED_SINCE"] == @asset.created_at.httpdate
       end
 
       def etag_match?(env)
-        env["HTTP_IF_NONE_MATCH"] == @etag
+        env["HTTP_IF_NONE_MATCH"] == @asset.etag
       end
 
       def not_modified_response(env)
@@ -54,35 +81,22 @@ module Assets
       end
 
       def ok_response(env)
-        [ 200, headers(env), [@source] ]
+        [ 200, headers(env), [@asset.source] ]
       end
-
 
       def headers(env)
         Hash.new.tap do |headers|
           headers["Content-Type"]   = "text/javascript"
-          headers["Content-Length"] = @source.size.to_s
+          headers["Content-Length"] = @asset.source.size.to_s
 
           headers["Cache-Control"]  = "public, must-revalidate"
-          headers["Last-Modified"]  = @previous_last_modified.httpdate
-          headers["ETag"]           = @etag
+          headers["Last-Modified"]  = @asset.created_at.httpdate
+          headers["ETag"]           = @asset.etag
 
-          if env["QUERY_STRING"] == @md5
+          if env["QUERY_STRING"] == @asset.md5
             headers["Cache-Control"] << ", max-age=#{1.year.to_i}"
           end
         end
-      end
-
-      def concate_source
-        @sources.collect { |file| File.read(file) }.join("\n\n")
-      end
-
-      def compute_md5
-        Digest::MD5.hexdigest(@source)
-      end
-
-      def quoted_md5
-        %("#{@md5}")
       end
   end
 end
